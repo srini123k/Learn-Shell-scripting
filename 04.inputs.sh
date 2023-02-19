bba13997b2b04f33f92368971636eebd6dd0265e
Srini K <srinikdevops0131@gmail.com>
2023-02-19 08:19:44 -0800

    updating ./Learn-Shell-scripting

--- 04.inputs.sh
+++ 04.inputs.sh
@@ -1,2 +1,12 @@
-echo $1
-echo $2
+#System variables -- 0-n, $*/$@, $# -- these are reserved for system
+
+echo $1 #First Argument
+echo $2 #Second Argument
+
+
+echo Script Name = $0 #Script Name
+
+echo All Arguments = $@ #All Arguments
+
+echo Number of Arguments = $# #Number of Arguments
+
